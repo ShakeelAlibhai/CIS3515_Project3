852fed6080c5704614db60fd4b7917037348f7d7
Shakeel Alibhai <shakeel.alibhai419@gmail.com>
2020-02-13 09:13:13 -0500

    Add spinner & work on ColorActivity class

--- app/src/main/java/edu/temple/coloractivityproject/ColorActivity.java
+++ app/src/main/java/edu/temple/coloractivityproject/ColorActivity.java
@@ -3,6 +3,7 @@ package edu.temple.coloractivityproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Spinner;
 
 public class ColorActivity extends AppCompatActivity {
 
@@ -10,5 +11,19 @@ public class ColorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_color);
+
+        Spinner s;
+        s = findViewById(R.id.spinner);
+
+        String colors[] = {
+                "Blue",
+                "Red",
+                "White",
+                "Yellow",
+                "Green"
+        };
+
+        ColorAdapter a = new ColorAdapter(this, colors);
+        s.setAdapter(a);
     }
 }
